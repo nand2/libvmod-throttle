9ad8b8bb5251b0082011774d8904329a4582628c
Martin Blix Grydeland <martin@varnish-software.com>
2011-02-09 16:16:29 +0100

    Out-of-tree vmod skeleton

---
+++ src/vmod.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+#include "vrt.h"
+#include "bin/varnishd/cache.h"
+
+#include "vcc_if.h"
+
+int
+init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
+{
+	return (0);
+}
+
+int
+vmod_abs(struct sess *sp, int val)
+{
+	return (abs(val));
+}
