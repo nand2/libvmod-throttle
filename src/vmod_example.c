ce3f10b6b95c0b32179130ecfb2bd82a670917d5
Martin Blix Grydeland <martin@varnish-software.com>
2011-05-25 15:40:16 +0200

    Rewrite the example to be a hello world vmod

--- src/vmod_example.c
+++ src/vmod_example.c
@@ -11,8 +11,22 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 	return (0);
 }
 
-int
-vmod_abs(struct sess *sp, int val)
+const char *
+vmod_hello(struct sess *sp, const char *name)
 {
-	return (abs(val));
+	char *p;
+	unsigned u, v;
+
+	u = WS_Reserve(sp->wrk->ws, 0); /* Reserve some work space */
+	p = sp->wrk->ws->f;		/* Front of workspace area */
+	v = snprintf(p, u, "Hello, %s", name);
+	v++;
+	if (v > u) {
+		/* No space, reset and leave */
+		WS_Release(sp->wrk->ws, 0);
+		return (NULL);
+	}
+	/* Update work space with what we've used */
+	WS_Release(sp->wrk->ws, v);
+	return (p);
 }
