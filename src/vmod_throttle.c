4313bc87bc468c467b309d64460f7ca42a3befca
Nicolas Deschildre <ndeschildre@gmail.com>
2012-10-06 16:50:45 +0200

    Ok logic working with limitation of sliding window of 1s/1mn/1h, tested with a single thread. No per-ip or per-key support yet.

--- src/vmod_throttle.c
+++ src/vmod_throttle.c
@@ -28,11 +28,57 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 	return (0);
 }
 
+
+// Private: Update the window counter (e.g. the last minute calls counter)
+void _vmod_update_window_counter(struct vmodth_call** last_window_call, int* window_counter, int window_length, double now) {
+  while((*last_window_call) && (*last_window_call)->time < now - window_length) {
+      (*last_window_call) = (*last_window_call)->prev;
+      (*window_counter)--;
+  }
+}
+
+// Private: Return the amount of time to wait to be allowed in this time window
+double _vmod_get_time_wait_for_window_compliance(int nb_window_call, int window_call_limit, struct vmodth_call* last_window_call, int window_length, double now) {
+  double result = 0.0;
+
+  if(nb_window_call >= window_call_limit) {
+    result = window_length - (now - last_window_call->time);
+  }
+
+  return result;
+}
+
+// Private: Update the window counter (e.g. the last minute calls counter)
+void _vmod_increment_window_counter(struct vmodth_call* new_call, struct vmodth_call** last_window_call, int* window_counter) {
+  (*window_counter)++;
+  if(*last_window_call == NULL) {
+    *last_window_call = new_call;
+  }  
+}
+
+// Private: Remove older entries
+void _vmod_remove_older_entries(struct vmodth_calls* calls, int max_window_length) {
+  struct vmodth_call *prev;
+
+  while(calls->last->prev) {
+    if(calls->last->time < calls->first->time - max_window_length) {
+      prev = calls->last->prev;
+      free(calls->last);
+      calls->last = prev;
+      prev->next = NULL;
+    }
+    else {
+      break;
+    }
+  }
+}
+
+// Public: is_allowed VCL command
 double
 vmod_is_allowed(struct sess *sp, struct vmod_priv *pc, struct sockaddr_storage *ip, const char* key, int call_limit_per_sec, int call_limit_per_min, int call_limit_per_hour)
 {
   struct vmodth_calls *calls;
-	struct vmodth_call* tmp;
+	double result = 0;
 
   //Init data structure
   calls = ((struct vmodth_calls*)pc->priv);
@@ -52,12 +98,38 @@ vmod_is_allowed(struct sess *sp, struct vmod_priv *pc, struct sockaddr_storage *
   }
 
   //Get time
+  //TODO: first a faster one, let's avoid a syscall
   struct timeval tv;
   gettimeofday(&tv, NULL);
+  double now = tv.tv_sec+tv.tv_usec/1000000.0;
 
+  //Update the windows counters and pointers, with the current time
+  _vmod_update_window_counter(&calls->last_call_sec, &calls->nb_calls_sec, 1, now);
+  _vmod_update_window_counter(&calls->last_call_min, &calls->nb_calls_min, 60, now);
+  _vmod_update_window_counter(&calls->last_call_hour, &calls->nb_calls_hour, 3600, now);
+
+  //Now lets check if we are allowed
+  //If we are not, let's return how much time we should wait before being allowed
+  double sec_win_wait = _vmod_get_time_wait_for_window_compliance(calls->nb_calls_sec, call_limit_per_sec, calls->last_call_sec, 1, now);
+  if(sec_win_wait > result) {
+    result = sec_win_wait;
+  }
+  double min_win_wait = _vmod_get_time_wait_for_window_compliance(calls->nb_calls_min, call_limit_per_min, calls->last_call_min, 60, now);
+  if(min_win_wait > result) {
+    result = min_win_wait;
+  }
+  double hour_win_wait = _vmod_get_time_wait_for_window_compliance(calls->nb_calls_hour, call_limit_per_hour, calls->last_call_hour, 3600, now);
+  if(hour_win_wait > result) {
+    result = hour_win_wait;
+  }
+  if(result > 0.0) {
+    return result;
+  }
+
+  //We are authorized
   //Add the call in the call list
   struct vmodth_call* new_call = malloc(sizeof(struct vmodth_call));
-  new_call->time = tv.tv_sec+tv.tv_usec/1000000.0;
+  new_call->time = now;
   new_call->next = calls->first;
   new_call->prev = NULL;
   if(calls->first) {
@@ -68,54 +140,13 @@ vmod_is_allowed(struct sess *sp, struct vmod_priv *pc, struct sockaddr_storage *
     calls->last = new_call;
   }
 
-  //Update the windows counters and pointers
-  _vmod_update_window_counter(new_call, &calls->last_call_sec, &calls->nb_calls_sec, 1);
-  _vmod_update_window_counter(new_call, &calls->last_call_min, &calls->nb_calls_min, 60);
-  _vmod_update_window_counter(new_call, &calls->last_call_hour, &calls->nb_calls_hour, 3600);
+  //Increment the windows counters and update if necessary their pointers
+  _vmod_increment_window_counter(new_call, &calls->last_call_sec, &calls->nb_calls_sec);
+  _vmod_increment_window_counter(new_call, &calls->last_call_min, &calls->nb_calls_min);
+  _vmod_increment_window_counter(new_call, &calls->last_call_hour, &calls->nb_calls_hour);
 
   //Remove the older entries older than the maximum window we are tracking, 1h
   _vmod_remove_older_entries(calls, 3600);
 
-  //Now lets check if we are allowed
-  //If we are not, let's return how much time we should wait before being allowed
-  double result = 0;
-  if(calls->nb_calls_sec > call_limit_per_sec) {
-    result = 1.0 - (new_call->time - calls->last_call_sec->time);
-  }
-
   return result;
-}
-
-// Update the window counter (e.g. the last minute calls counter)
-void _vmod_update_window_counter(struct vmodth_call* new_call, struct vmodth_call** last_window_call, int* window_counter, int window_length) {
-  (*window_counter)++;
-  if(*last_window_call == NULL) {
-    *last_window_call = new_call;
-  }
-  while((*last_window_call)->prev) {
-    if((*last_window_call)->time < new_call->time - window_length) {
-      (*last_window_call) = (*last_window_call)->prev;
-      (*window_counter)--;
-    }
-    else {
-      break;
-    }
-  }  
-}
-
-//Remove older entries
-void _vmod_remove_older_entries(struct vmodth_calls* calls, int max_window_length) {
-  struct vmodth_call *prev;
-
-  while(calls->last->prev) {
-    if(calls->last->time < calls->first->time - max_window_length) {
-      prev = calls->last->prev;
-      free(calls->last);
-      calls->last = prev;
-      prev->next = NULL;
-    }
-    else {
-      break;
-    }
-  }
 }
